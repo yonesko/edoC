d36ee1f83f253743a9b94d895da4a5287947f868
Glebushka <yonesko@gmail.com>
2015-10-25 20:16:03 +0300

    telefon 100% USPEH

--- Telefon_Numbers/src/gleb/Main.java
+++ Telefon_Numbers/src/gleb/Main.java
@@ -7,75 +7,63 @@ public class Main {
     static BufferedReader bfNambas;
     static List<String> lNambas;
     static List<Tree> forest;
-    static List<String> overlapHeap;
-    static List<List<String>> overlapFrst;
 
     public static void main(String[] args) throws IOException {
-        bfNambas = new LineNumberReader(new FileReader("resources/fileservlet.txt"));
+        bfNambas = new LineNumberReader(new FileReader("resources/fileservlet2.txt"));
         lNambas = new ArrayList<>(Integer.parseInt(bfNambas.readLine()));
         forest = new ArrayList<>();
-        overlapHeap = new ArrayList<>();
-        overlapFrst = new ArrayList<>();
         {
             String s;
             while ((s = bfNambas.readLine()) != null)
                 lNambas.add(s);
         }
         Collections.sort(lNambas);
-        for (String lNamba : lNambas) {
-            System.out.println(lNamba);
-        }
+//        for (String lNamba : lNambas) {
+//            System.out.println(lNamba);
+//        }
         System.out.println();
 
-        deep(null, 0, lNambas.size());
+        deep(null, 0, lNambas.size(), 0);
 
+        int sum = 0;
         for (Tree tree : forest) {
-            System.out.println(tree);
+//            tree.show();
+            sum += tree.getElementsCount(0);
         }
+        System.out.println(sum);
     }//main
 
-    static void deep(Tree root, int start, int end) {
-        System.out.println("deep " + root + " " + start + " " + end);
-        int offset = getOffset(root);
-        List<String> minPrefix = new ArrayList<>();
-        minPrefix.add(lNambas.get(start).substring(offset));
+    static void deep(Tree root, int start, int end, int offset) {
+//        System.out.println("deep " + root + "[ " + start + "; " + end + " ) " + offset);
+        String minOverlap = lNambas.get(start).substring(offset);
+
+        if (end <= start + 1 && root != null)
+            return;
 
         for (int i = start + 1; i < end; i++) {
             String curName = lNambas.get(i).substring(offset);
-            String lastPrefix = minPrefix.get(minPrefix.size() - 1);
-            String curPrefix = leftOverlap(curName, lastPrefix);
+            String curOverlap = leftOverlap(minOverlap, curName);
 
-            if (curPrefix.length() == 0)
-                minPrefix.add(curName);
-            else if (curPrefix.length() < lastPrefix.length())
-                minPrefix.set(minPrefix.size() - 1, curPrefix);
-        }
-        System.out.println(minPrefix);
-    }
-
-    static Tree getTree(String name) {
-        for (Tree tree : forest)
-            if (tree.getName().equals(name))
-                return tree;
-        return null;
-    }
-    static void addTree(String name, Tree root) {
-        forest.add(new Tree(name, root));
-    }
-    static int getOffset(Tree t) {
-        int result = 0;
-        if (t != null)
-            while ( t != null) {
-                result += t.getName().length();
-                t = t.getParent();
+            if (curOverlap.length() == 0) {
+                Tree t = new Tree(minOverlap);
+                addTree(t, root);
+                minOverlap = curName;
+                deep(t, start, i, offset + t.getName().length());
+                start = i;
             }
-        return result;
+            else if (curOverlap.length() < minOverlap.length())
+                minOverlap = curOverlap;
+        }
+        Tree t = new Tree(minOverlap);
+        addTree(t, root);
+        deep(t, start, end, offset + minOverlap.length());
     }
-    static String cutPrefix(String prefix, String s) {
-            return s.substring(prefix.length());
-    }
-    static String cutPrefix(StringBuilder prefix, String s) {
-        return cutPrefix(new String(prefix), s);
+
+    static void addTree(Tree t, Tree root) {
+        if (root == null)
+            forest.add(t);
+        else
+            root.addChild(t);
     }
     static String leftOverlap(String a, String b) {
         StringBuilder result = new StringBuilder();
@@ -91,43 +79,43 @@ public class Main {
 
 class Tree {
     private String name;
-    private Tree parent;
-    private int start;
-    private int end;
+    private List<Tree> children;
 
-    public Tree getParent() {
-        return parent;
+    void addChild(Tree tree) {
+        children.add(tree);
     }
-
-
-
-    Tree (String name, Tree parent) {
+    public Tree(String name) {
         this.name = name;
-        this.parent = parent;
-
+        children = new ArrayList<>();
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setParent(Tree parent) {
-        this.parent = parent;
-    }
-
     public String getName() {
         return name;
+    }
+    private void show(int indent) {
+        char[] t = new char[indent];
+        Arrays.fill(t, '\t');
+        System.out.println(new String(t) + (name.length() == 0 ? '.' : name));
+        for (Tree child : children) {
+            child.show(indent + 1);
+        }
+
+    }
+    void show() {
+        show(0);
     }
 
     @Override
     public String toString() {
         return "Tree{" +
                 "name='" + name + '\'' +
-                ", parent=" + (parent == null ? null : parent.name) +
                 '}';
     }
 
-    void show() {
-
+    int getElementsCount(int countStart) {
+        countStart = name.length();
+        for (Tree child : children) {
+            countStart += child.getElementsCount(countStart);
+        }
+        return countStart;
     }
 }
